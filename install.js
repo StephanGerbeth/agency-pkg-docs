a5f8fc10387dc7247dc901381469f4b76983a6a7
Thorn Walli <thorn.walli@grabarzundpartner.intra>
2016-07-08 18:35:59 +0200

    updated install.js

--- install.js
+++ install.js
@@ -1,7 +1,7 @@
 /**
  * Copy js source files to root from installed package, for use relative path with require.
- * require("agency-pkg-name/relative/path");
- * @version 0.0.1
+ * require("prefix-pkg-name/relative/path");
+ * @version 0.0.2
  */
 
 "use strict";
