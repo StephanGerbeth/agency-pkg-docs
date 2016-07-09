4a42d41163c31316ce3ec07be258e09a81f9354a
Thorn Walli <thorn.walli@grabarzundpartner.intra>
2016-07-09 21:49:55 +0200

    styling

--- src/js/partials/components/Overview.js
+++ src/js/partials/components/Overview.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var history = require('agency-pkg-services/history');
 var Controller = require('agency-pkg-base/Controller');
 var DomModel = require('agency-pkg-base/DomModel');
 var dataTypeDefinition = require('agency-pkg-base/dataTypeDefinition');
@@ -36,12 +37,25 @@ module.exports = Controller.extend({
         Controller.prototype.initialize.apply(this, arguments);
         this.model.on('change:showOverlay', onChangeShowOverlay.bind(this));
         this.model.on('change:showMenu', onChangeShowMenu.bind(this));
+        this.model.on('change:url', onChangeUrl.bind(this));
 
         onChangeShowOverlay.bind(this)(this.model, this.model.showOverlay);
         onChangeShowMenu.bind(this)(this.model, this.model.showMenu);
+
+        if (history.registry.get('url')) {
+            this.model.url = history.registry.get('url').value;
+        }
+
     }
 
 });
+
+function onChangeUrl(model, url) {
+    history.update([{
+        name: 'url',
+        value: encodeURIComponent(url)
+    }]);
+}
 
 function onChangeShowOverlay(model, showOverlay) {
     this.el.classList.toggle('js-show-overlay', showOverlay);
--- src/js/partials/fragments/Menu.js
+++ src/js/partials/fragments/Menu.js
@@ -3,7 +3,6 @@
 var Controller = require('agency-pkg-base/Controller');
 var DomModel = require('agency-pkg-base/DomModel');
 var dataTypeDefinition = require('agency-pkg-base/dataTypeDefinition');
-
 module.exports = Controller.extend({
 
     modelConstructor: DomModel.extend(dataTypeDefinition, {
@@ -11,21 +10,44 @@ module.exports = Controller.extend({
     }),
 
     events: {
-        'click [href][target]': onClick,
+        'click [data-partial="elements/link/menu"]': onClick,
         'click .js-click-toggle': onClickToggle
     },
 
     initialize: function() {
         Controller.prototype.initialize.apply(this, arguments);
+
+        this.links = this.el.querySelectorAll('[data-partial="elements/link/menu"]');
+
+        this.targetModel.on('change:url', onUrlChange.bind(this));
+        if (this.targetModel.url) {
+            onUrlChange.bind(this)(this.targetModel, this.targetModel.url);
+        }
     },
 
 });
 
-function onClick() {
+function onClick(e) {
+    e.preventDefault();
     this.targetModel.showOverlay = true;
+    this.targetModel.url = e.target.href;
 }
 
 function onClickToggle(e) {
     e.preventDefault();
     this.targetModel.showMenu = !this.targetModel.showMenu;
+}
+
+function onUrlChange(model, url) {
+    if (this.currentItemEl) {
+        this.currentItemEl.classList.remove('selected');
+    }
+    console.log(this.links);
+    this.links.forEach(function(linkEl) {
+        if (linkEl.href === url) {
+            this.currentItemEl = linkEl;
+            this.currentItemEl.classList.add('selected');
+            return true;
+        }
+    }.bind(this));
 }
