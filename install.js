dc301f67b75e279bd410991da36988acd205577f
Thorn Walli <thorn.walli@grabarzundpartner.intra>
2016-07-11 23:07:25 +0200

    updated install.js stuff

--- install.js
+++ install.js
@@ -1,7 +1,7 @@
 /**
  * Copy js source files to root from installed package, for use relative path with require.
  * require("prefix-pkg-name/relative/path");
- * @version 0.0.3
+ * @version 0.0.4
  */
 
 "use strict";
@@ -21,8 +21,8 @@ function isDependencyPackage(cb) {
  * Clean package root and exclude src/js
  */
 function preparePackage() {
-    var ignoreFiles = ['package.json', 'README.md', 'LICENSE.md', '.gitignore', '.npmignore', '.editorconfig', 'index.js', 'index.pcss'];
-    var ignoreSrcJsFiles = ['main.js', 'packages.js'];
+    var ignoreFiles = ['package.json', 'README.md', 'LICENSE.md', '.gitignore', '.npmignore', 'index.js', 'index.pcss'];
+    var ignoreCopyFiles = [];
     var copyRecursiveSync = function(src, dest) {
         var exists = fs.existsSync(src);
         var stats = exists && fs.statSync(src);
@@ -36,20 +36,39 @@ function preparePackage() {
                     path.join(dest, filename));
             });
         } else {
-            if (ignoreSrcJsFiles.indexOf(path.basename(src)) === -1) {
+            if (ignoreCopyFiles.indexOf(path.basename(src)) === -1) {
                 fs.linkSync(src, dest);
             }
         }
     };
-    var src = path.join(process.cwd(), 'src', 'js');
+    var src = path.join(process.cwd(), 'src-pkg');
     if (fs.existsSync(src)) {
         fs.readdirSync(process.cwd()).forEach(function(filename) {
             var curPath = path.join(process.cwd(), filename);
             if (ignoreFiles.indexOf(path.basename(curPath)) === -1 && fs.statSync(curPath).isFile()) {
                 fs.unlinkSync(curPath);
+            } else if (ignoreFiles.indexOf(path.basename(curPath)) === -1 && fs.statSync(curPath).isDirectory()) {
+                deleteFolderRecursive(curPath);
             }
         });
         copyRecursiveSync(src, process.cwd());
+    }
+}
+
+/**
+ * Delete directories recursive.
+ */
+function deleteFolderRecursive(path) {
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function(file) {
+            var curPath = path + "/" + file;
+            if (fs.lstatSync(curPath).isDirectory()) {
+                deleteFolderRecursive(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
     }
 }
 
